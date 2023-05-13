d4b7bd5c66ae0f134bd6b71fba3c832e659603fc
Leo Blanchette <theswitchboard@protonmail.com>
2023-05-13 06:12:26 -0500

    Commiting...

--- Editor/GlobalSettings/UnturnedGetInstallDirectoryWindow.cs
+++ Editor/GlobalSettings/UnturnedGetInstallDirectoryWindow.cs
@@ -2,15 +2,18 @@ using System.IO;
 using UnityEngine;
 using UnityEditor;
 
-public class GetInstallDirectoryWindow : EditorWindow
-{    
-    public static string GetUnturnedInstallationPath()
+namespace LB3D.PuggosWorld.Unturned
+{
+    public class GetInstallDirectoryWindow : EditorWindow
     {
-        string path = EditorUtility.OpenFolderPanel("Overwrite with png", "", "png");
-        if (path.Length != 0)
-        {            
-            return path;
+        public static string GetUnturnedInstallationPath()
+        {
+            string path = EditorUtility.OpenFolderPanel("Overwrite with png", "", "png");
+            if (path.Length != 0)
+            {
+                return path;
+            }
+            return null;
         }
-        return null;
     }
 }
--- Runtime/Scripts/ModGenerator/ReadOnlyAttribute.cs
+++ Runtime/Scripts/ModGenerator/ReadOnlyAttribute.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-/// <summary>
-/// https://web.archive.org/web/20230512165644/https://www.patrykgalach.com/2020/01/20/readonly-attribute-in-unity-editor/
-/// Read Only attribute.
-/// Attribute is use only to mark ReadOnly properties.
-/// </summary>
-public class ReadOnlyAttribute : PropertyAttribute { }
+namespace LB3D.PuggosWorld.Unturned
+{
+    /// <summary>
+    /// https://web.archive.org/web/20230512165644/https://www.patrykgalach.com/2020/01/20/readonly-attribute-in-unity-editor/
+    /// Read Only attribute.
+    /// Attribute is use only to mark ReadOnly properties.
+    /// </summary>
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}
